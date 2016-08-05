a1e5742d71e01f833e9ac96af9f715a14d9112ae
Dave Tamayo <djbtamayo@gmail.com>
2016-08-04 21:46:20 -0400

    Imported in the Google Maps package from the Asset Store. Created some tests using Unity's Location Services Started messing around with how to use together Google Maps with Unity's Location Services.

---
+++ Assets/Scripts/GPSLocationServices.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GPSLocationServices : MonoBehaviour {
+
+    const float EARTH_RADIUS = 6371000; // meters
+
+
+    protected float HaversineDistance (ref float lastLatitude, ref float lastLongitude) {
+
+        float newLatitude = Input.location.lastData.latitude;
+        float newLongitude = Input.location.lastData.longitude;
+
+        float deltaLatitude = (newLatitude - lastLatitude) * Mathf.Deg2Rad;
+        float deltaLongitude = (newLongitude - lastLongitude) * Mathf.Deg2Rad;
+
+        float distance = 2 * EARTH_RADIUS * Mathf.Asin(Mathf.Sqrt(Mathf.Sin(deltaLatitude * 0.5f) * Mathf.Sin(deltaLatitude * 0.5f)
+            + Mathf.Cos(lastLatitude * Mathf.Deg2Rad) * Mathf.Cos(newLatitude * Mathf.Deg2Rad) * Mathf.Sin(deltaLongitude * 0.5f) * Mathf.Sin(deltaLongitude * 0.5f)));
+
+        lastLatitude = newLatitude;
+        lastLongitude = newLongitude;
+
+        return distance;
+    }
+}
---
+++ Assets/Scripts/GPSManualUpdate.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GPSManualUpdate : GPSLocationServices {
+
+    const float EARTH_RADIUS = 6371000; // meters
+
+    [SerializeField] Text lastLatitudeText;
+    [SerializeField] Text lastLongitudeText;
+    [SerializeField] Text newLatitudeText;
+    [SerializeField] Text newLongitudeText;
+    [SerializeField] Text haversineText;
+
+    float lastLatitude;
+    float lastLongitude;
+
+    void Awake () {
+
+        StopCoroutine(InitializeLocationServices());
+        StartCoroutine(InitializeLocationServices());
+    }
+
+    IEnumerator InitializeLocationServices () {
+
+        #if UNITY_IOS
+        if(Input.location.isEnabledByUser) {
+
+            Input.location.Start();
+
+            while(Input.location.status == LocationServiceStatus.Initializing) {
+
+                yield return null;
+            }
+
+            lastLatitude = Input.location.lastData.latitude;
+            lastLongitude = Input.location.lastData.longitude;
+
+            lastLatitudeText.text = "Last Latitude: " + lastLatitude;
+            lastLongitudeText.text = "Last Longitude: " + lastLongitude;
+        }
+        #endif
+
+        yield return null;
+    }
+
+    public void RecalculatePosition () {
+
+        #if UNITY_IOS
+        if(Input.location.isEnabledByUser && Input.location.status == LocationServiceStatus.Running) {
+
+            lastLatitudeText.text = "Last Latitude: " + lastLatitude;
+            lastLongitudeText.text = "Last Longitude: " + lastLongitude;
+
+            newLatitudeText.text = "New Latitude: " + Input.location.lastData.latitude;
+            newLongitudeText.text = "New Longitude: " + Input.location.lastData.longitude;
+
+            haversineText.text = "Haversine: " + HaversineDistance(ref lastLatitude, ref lastLongitude);
+        }
+        #endif
+    }
+}
