2aeda1e9854c5a55512ae792bee01087919907dc
Dave Tamayo <djbtamayo@gmail.com>
2016-08-26 17:18:08 -0400

    Polishing and debugging for the demo on the weekend.

---
+++ Assets/Scripts/MainMenuNameUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class MainMenuNameUI : MonoBehaviour {
+
+    [SerializeField] Text nameText;
+
+
+    void Awake () {
+
+        if(nameText) { nameText.text = PlayerData.GetPlayerName(); }
+    }
+}
