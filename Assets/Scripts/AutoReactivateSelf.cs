c249eae0631f45a375304ce4872e6b2f8fb18136
Lennard Spokowski <lennard.spokowski@gmail.com>
2016-09-13 12:17:06 -0400

    -Added temp Ouistiti png -Created particle false reactive script -Moved some UI elements in TUTOR menu -Adjusted Ping particle effect.

---
+++ Assets/Scripts/AutoReactivateSelf.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoReactivateSelf : MonoBehaviour {
+
+    [SerializeField] float reactivationTime = 0.5f;
+
+    void OnEnable () {
+
+        StopCoroutine(DelayReactivation());
+        StartCoroutine(DelayReactivation());
+    }
+
+    IEnumerator DelayReactivation () {
+
+        yield return new WaitForSeconds(reactivationTime);
+
+        this.gameObject.SetActive(true);
+    }
+}
